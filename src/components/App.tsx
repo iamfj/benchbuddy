c4f1e50a6c37110e8a4e5f875243e36ec4fe19cd
Fabian Jocks <dev@jocks.io>
2020-08-18 13:58:36 +0200

    Cleanup code

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,9 @@
-import React from "react";
-import {Component} from "react";
+import React, {Component} from "react";
 
 export default class App extends Component<any, any> {
-  render() {
-    return <div>
-      <h1>Hello World!</h1>
-    </div>;
-  }
+    render() {
+        return <div>
+            <h1>Hello World!</h1>
+        </div>;
+    }
 }
