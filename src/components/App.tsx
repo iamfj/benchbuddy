47de66f02459bd62b72a8bd3f5211365ea026089
Fabian Jocks <dev@jocks.io>
2020-08-18 12:13:19 +0200

    Added hello world component

---
+++ src/components/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import {Component} from "react";
+
+export default class App extends Component<any, any> {
+  render() {
+    return <div>
+      <h1>Hello World!</h1>
+    </div>;
+  }
+}
