bda8730d4eead1afa23f248baab9c4e63a60b3b1
Fabian Jocks <dev@jocks.io>
2020-10-20 10:44:36 +0200

    Added bottom navigation layout

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,37 @@
 import React, { Component, ReactNode } from "react";
-import { Button } from "@material-ui/core";
+import { Box, BottomNavigation, BottomNavigationAction } from "@material-ui/core";
+import RestoreIcon from "@material-ui/icons/Restore";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import LocationOnIcon from "@material-ui/icons/LocationOn";
 
-export default class App extends Component<unknown, unknown> {
-  render(): ReactNode {
+interface AppState {
+  value: number;
+}
+
+export default class App extends Component<unknown, AppState> {
+  public constructor(props: unknown) {
+    super(props);
+
+    this.setState({
+      value: 0,
+    });
+  }
+
+  public render(): ReactNode {
     return (
-      <div>
-        <Button color="primary">Hello World</Button>
-      </div>
+      <Box height="100%">
+        <BottomNavigation
+          value={0}
+          onChange={(event, newValue: number) => {
+            this.setState({ value: newValue });
+          }}
+          showLabels
+        >
+          <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+          <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+          <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+        </BottomNavigation>
+      </Box>
     );
   }
 }
